bf864505bf9e771e455351435f5394b19878d80d
MuktharAmiyan <amiyanmukthar@gmail.com>
2024-02-24 11:27:13 +0530

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Mukthar Amiyan on 24/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
