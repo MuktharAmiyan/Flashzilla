741146893267c132845da1fb3dca2d882eed6daf
MuktharAmiyan <amiyanmukthar@gmail.com>
2024-02-24 11:59:05 +0530

    How to use gestures in SwiftUI

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var offSet = CGSize.zero
+    @State private var isDragging = false
+      
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        let dragGesture = DragGesture()
+            .onChanged { value in
+                offSet = value.translation
+            }.onEnded { _ in
+                withAnimation {
+                    offSet = .zero
+                    isDragging = false
+                }
+            }
+        
+        let pressGesture = LongPressGesture()
+            .onEnded { _ in
+                withAnimation {
+                    isDragging = true
+                }
+            }
+        let compined = pressGesture.sequenced(before: dragGesture)
+        
+            Circle()
+            .fill(.red)
+            .frame(width: 64,height: 64)
+            .scaleEffect(isDragging ? 1.5 : 1)
+            .offset(offSet)
+            .gesture(compined)
     }
 }
 
